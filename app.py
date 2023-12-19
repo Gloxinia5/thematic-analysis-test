cc67215de0042b0e172bc8b5bd8740294a0917c3
Gloxinia5 <punciaaron09@gmail.com>
2023-12-19 19:42:04 +0800

    CHANGE

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return requests.__version__
+   
 
 if __name__ == '__main__':
     app.run()
