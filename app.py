c93f6a59bc54b240e96221463404d194c7ddf907
Gloxinia5 <punciaaron09@gmail.com>
2023-12-19 19:47:08 +0800

    test

--- app.py
+++ app.py
@@ -3,9 +3,33 @@ import requests
 
 app = Flask(__name__)
 
+def prompt(message):
+    headers = {
+        'Authorization': 'Bearer pk-ksKYKKFkBJEcejWNEAtiLAtbIbnXmkpqQMYotASZfVZuSfqk',
+        'Content-Type': 'application/json'
+    }
+
+    json_data = {
+        'model': 'pai-001-light-beta',
+        'max_tokens': 2000,
+        'messages': [
+            {
+                'role': 'system',
+                'content': 'You are an helpful assistant.',
+            },
+            {
+                'role': 'user',
+                'content': message,
+            },
+        ],
+    }
+
+    response = requests.post('https://api.pawan.krd/v1/chat/completions', headers=headers, json=json_data)
+    return response.json()
+
 @app.route('/')
 def hello_world():
-    return requests.__version__
+    return prompt("how are you?")
    
 
 if __name__ == '__main__':
